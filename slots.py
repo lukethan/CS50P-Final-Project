a88b471aebb21063c192686ab219b735acd2f50a
lukethan <92878195+lukethan@users.noreply.github.com>
2024-08-16 13:03:35 -0400

    Update slots.py

--- slots.py
+++ slots.py
@@ -5,16 +5,15 @@ from roulette_class import Roulette
 import os
 
 def main():
-    print("\n\nWelcome to CS_ino_50!", end="\n\n\n")
+    print("\n********************\nWelcome to CS_ino_50!", end="\n********************\n\n")
     p1 = Player.get()
     r1 = Roulette()
     s1 = Slot()
-    p1.game()
     print(p1)
+    p1.game()
     while True:
         try:
             while p1.gamechoice == 1:
-                # while True:
                     p1.r_bet()
                     p1.bet()
                     amount = p1.amount
@@ -53,32 +52,6 @@ def main():
 
 
 
-
-
-# def roulette():
-#     print(p1)
-#     p1.bet()
-#     amount = p1.amount
-#     name = p1.name
-#     num = p1.get_num()
-#     r1.bet_num(amount, name, num)
-#     p1.check(r1.winnings)
-
-    # p1 = Player.get()
-    # s1 = Slot()
-    # print(p1)
-    # while True:
-    #     try:
-    #         p1.bet()
-    #         amount = p1.amount
-    #         name = p1.name
-    #         s1.play(amount, name)
-    #         p1.check(s1.winnings)
-
-    #     except EOFError:
-    #         print("\nThanks for playing")
-    #         return False
-
-
 if __name__=="__main__":
     main()
+
