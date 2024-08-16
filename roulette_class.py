e7ffb42d9a34751dc9e6598071ea10161d0e1d4b
lukethan <92878195+lukethan@users.noreply.github.com>
2024-08-16 13:04:30 -0400

    Update roulette_class.py

--- roulette_class.py
+++ roulette_class.py
@@ -8,13 +8,26 @@ class Roulette:
         self.color = None
         self.winnings = 0
 
+    def testevenwin(self,amount,name):
+        color = "🔴"
+        number = 6
+        self.spin()
+        os.system('clear')
+        print(f"\n\n{color}:{number}")
+        if number % 2 == 0:
+            self.winnings = amount+amount
+            print(f"Good job {name}, you won {self.winnings}!")
+        else:
+            print("Better luck next time!")
+            self.winnings = 0
+
     def bet_even(self,amount,name):
         self.spin()
         os.system('clear')
         print(f"\n\n{self.color}:{self.number}")
         if self.number % 2 == 0:
             self.winnings = amount+amount
-            print(f"Good job {name}, you won {self.winnings}!")
+            print(f"Good job {name}, you won ${self.winnings}!")
         else:
             print("Better luck next time!")
             self.winnings = 0
@@ -26,9 +39,9 @@ class Roulette:
         print(f"\n\n{self.color}:{self.number}")
         if self.number % 2 != 0:
             self.winnings = amount*2
-            print(f"Good job {name}, you won {self.winnings}!")
+            print(f"\nGood job {name}, you won ${self.winnings}!")
         else:
-            print("Better luck next time!")
+            print("\nBetter luck next time!")
             self.winnings = 0
 
 
@@ -39,9 +52,9 @@ class Roulette:
         print(f"\n\n{self.color}:{self.number}")
         if self.color == "🔴":
             self.winnings = amount*2
-            print(f"Good job {name}, you won {self.winnings}!")
+            print(f"\nGood job {name}, you won ${self.winnings}!")
         else:
-            print("Better luck next time!")
+            print("\nBetter luck next time!")
             self.winnings = 0
 
 
@@ -52,9 +65,9 @@ class Roulette:
         print(f"\n\n{self.color}:{self.number}")
         if self.color != "🔴":
             self.winnings = amount*2
-            print(f"Good job {name}, you won {self.winnings}!")
+            print(f"\nGood job {name}, you won ${self.winnings}!")
         else:
-            print("Better luck next time!")
+            print("\nBetter luck next time!")
             self.winnings = 0
 
     def bet_num(self,amount,name,number):
@@ -63,14 +76,11 @@ class Roulette:
         print(f"\n\n{self.color}:{self.number}")
         if number == self.number:
             self.winnings = amount*35
-            print(f"Good job {name}, you won {self.winnings}!")
+            print(f"\nGood job {name}, you won ${self.winnings}!")
         else:
-            print("Better luck next time!")
+            print("\nBetter luck next time!")
             self.winnings = 0
 
-    # def bet_color(self,balance,amount,name):
-    #     ...
-    # def bet_number(self,balance,amount,name):
 
     def spin(self):
         self.number = random.randint(0,38)
